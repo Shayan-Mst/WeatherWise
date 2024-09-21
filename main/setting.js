24be78d81d768e8eb824642bd79fafa1a27d7792
Shayan-Mst <shnbackup007@gmail.com>
2024-09-21 15:39:49 +0330

    setting prefrence done

--- main/setting.js
+++ main/setting.js
@@ -1,3 +1,81 @@
+
+//onload user prefrence
+window.addEventListener('load', () => {
+
+    let lat = localStorage.getItem('lat');
+    let lon = localStorage.getItem('lon');
+    let notif = localStorage.getItem('notif')
+    let temperature = localStorage.getItem('temperature')
+    let wind = localStorage.getItem('wind')
+    let pressure = localStorage.getItem('pressure')
+    let precipitation = localStorage.getItem('precipitation')
+    let distance =  localStorage.getItem('distance')
+
+    
+   
+    //location
+    if(lat && lon){
+     location_lat.checked = true;
+    }
+    else{
+    location_lat.checked = false;
+
+    }
+
+    //notif
+    if(notif == "true"){
+
+        notifications.checked=true
+    }
+    else{
+
+        notifications.checked = false
+    }
+
+    //temperature
+
+    if(temperature == "fahrenheit"){
+
+        Fahrenheit.classList.add('setting--selected--item')
+       
+    }
+    else{
+        Celsius.classList.add('setting--selected--item')
+    }
+
+    //wind speed
+    if(wind == "ms"){
+        Ms.classList.add('setting--selected--item')
+    }
+    else if(wind == 'knots'){
+        Knots.classList.add('setting--selected--item')
+    }
+    else{
+        Kmh.classList.add('setting--selected--item')
+    }
+
+    //pressure
+
+    if(pressure == "inches") Inches.classList.add('setting--selected--item')
+    else if(pressure == "mm") Mm.classList.add('setting--selected--item')
+    else if(pressure == "hpa") hPa.classList.add('setting--selected--item')
+    else kPa.classList.add('setting--selected--item')
+
+    //precipitation
+
+    if(precipitation == 'inch') Inch.classList.add('setting--selected--item')
+    else Milimeter.classList.add('setting--selected--item')
+
+    //distance
+
+    if(distance == "mile") Mile.classList.add('setting--selected--item')
+    else Km.classList.add('setting--selected--item')
+    
+  });
+
+
+//Location
+
 // Function to get user location
 function getLocation() {
     if (navigator.geolocation) {
@@ -45,32 +123,9 @@ function getLocation() {
     
   })
 
-  window.addEventListener('load', () => {
-
-    let lat = localStorage.getItem('lat');
-    let lon = localStorage.getItem('lon');
-    let notif = localStorage.getItem('notif')
-   
-    if(lat && lon){
-     location_lat.checked = true;
-    }
-    else{
-    location_lat.checked = false;
-
-    }
-    if(notif == "true"){
-
-        notifications.checked=true
-    }
-    else{
-
-        notifications.checked = false
-    }
-    
-  });
 
 
-
+//notification
    // Function to ask for permission and send a notification
    function sendNotification() {
     // Check if the browser supports notifications
@@ -93,3 +148,133 @@ function getLocation() {
       });
     }
   }
+
+
+  //temperature
+
+  let Celsius=document.getElementById('Celsius');
+  let Fahrenheit = document.getElementById('Fahrenheit')
+
+  function celsius(){
+   
+   Celsius.classList.add('setting--selected--item')
+   Fahrenheit.classList.remove('setting--selected--item')
+   localStorage.setItem('temperature','celsius')
+
+  }
+
+  function fahrenheit(){
+   
+    Celsius.classList.remove('setting--selected--item')
+    Fahrenheit.classList.add('setting--selected--item')
+    localStorage.setItem('temperature','fahrenheit')
+ 
+   }
+
+   //wind speed
+
+   let Kmh = document.getElementById('kmh')
+   let Ms = document.getElementById('ms')
+   let Knots = document.getElementById('knots')
+
+   function kmh(){
+
+    Kmh.classList.add('setting--selected--item')
+    Ms.classList.remove('setting--selected--item')
+    Knots.classList.remove('setting--selected--item')
+    localStorage.setItem('wind','kmh')
+
+   }
+
+   function ms(){
+    Ms.classList.add('setting--selected--item')
+    Knots.classList.remove('setting--selected--item')
+    Kmh.classList.remove('setting--selected--item')
+    localStorage.setItem('wind','ms')
+   }
+
+   function knots(){
+
+    Knots.classList.add('setting--selected--item')
+    Kmh.classList.remove('setting--selected--item')
+    Ms.classList.remove('setting--selected--item')
+    localStorage.setItem('wind','knots')
+   }
+
+   //pressure
+
+   let Inches = document.getElementById("inches")
+   let Mm = document.getElementById("mm")
+   let hPa = document.getElementById("hPa")
+   let kPa = document.getElementById("kPa")
+
+
+   function inches(){
+    Inches.classList.add('setting--selected--item')
+    Mm.classList.remove('setting--selected--item')
+    kPa.classList.remove('setting--selected--item')
+    hPa.classList.remove('setting--selected--item')
+    localStorage.setItem('pressure','inches')
+
+   }
+
+   function mm(){
+    Mm.classList.add('setting--selected--item')
+    Inches.classList.remove('setting--selected--item')
+    hPa.classList.remove('setting--selected--item')
+    kPa.classList.remove('setting--selected--item')
+    localStorage.setItem('pressure','mm')
+   }
+   function hpa(){
+    hPa.classList.add('setting--selected--item')
+    kPa.classList.remove('setting--selected--item')
+    Inches.classList.remove('setting--selected--item')
+    Mm.classList.remove('setting--selected--item')
+    localStorage.setItem('pressure','hpa')
+
+   }
+   function kpa(){
+
+    kPa.classList.add('setting--selected--item')
+    Inches.classList.remove('setting--selected--item')
+    hPa.classList.remove('setting--selected--item')
+    Mm.classList.remove('setting--selected--item')
+    localStorage.setItem('pressure','kpa')
+   }
+
+
+//  PRECIPITATION
+
+let Inch = document.getElementById("inch")
+let Milimeter = document.getElementById("milimeters")
+
+function inch(){
+    Inch.classList.add('setting--selected--item')
+    Milimeter.classList.remove('setting--selected--item')
+    localStorage.setItem('precipitation','inch')
+}
+
+function milimeter(){
+    Milimeter.classList.add('setting--selected--item')
+    Inch.classList.remove('setting--selected--item')
+    localStorage.setItem('precipitation','milimeter')
+}
+
+//distance
+
+let Km = document.getElementById("km")
+let Mile = document.getElementById("mile")
+function km(){
+
+    Km.classList.add('setting--selected--item')
+    Mile.classList.remove('setting--selected--item')
+    localStorage.setItem('distance','km')
+
+}
+
+function mile(){
+    Km.classList.remove('setting--selected--item')
+    Mile.classList.add('setting--selected--item')
+    localStorage.setItem('distance','mile')
+
+}
